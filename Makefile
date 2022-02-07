afdadbe2468187159ddf0c0667e8ae3c7011c060
Simon Richardson <stickupkid@gmail.com>
2022-02-07 09:59:38 +0000

    Scaffold the initial dqlite packages
    This includes the repl.

---
+++ Makefile
@@ -0,0 +1,43 @@
+PROJECT_PATH=$(GOPATH)/src/github.com/SimonRichardson/nu-juju-data
+
+DQLITE_S3_BUCKET=s3://dqlite-static-libs
+DQLITE_S3_ARCHIVE_NAME=$(shell date -u +"%Y-%m-%d")-dqlite-deps-$(shell uname -m).tar.bz2
+DQLITE_S3_ARCHIVE_PATH=${DQLITE_S3_BUCKET}/${DQLITE_S3_ARCHIVE_NAME}
+
+DQLITE_EXTRACTED_DEPS_PATH=${PROJECT_PATH}/_deps
+DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH=${DQLITE_EXTRACTED_DEPS_PATH}/juju-dqlite-static-lib-deps
+
+dqlite-deps-check:
+	@if [ ! -d ${DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH} ]; then \
+		$(MAKE) -s dqlite-deps-pull; \
+	fi
+
+dqlite-deps-pull:
+	@echo "DQLITE: Cleaning up deps path"
+	@mkdir -p ${DQLITE_EXTRACTED_DEPS_PATH}
+	@rm -rf ${DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH}
+	@echo "DQLITE: Pulling latest-juju-dqlite-static-lib-deps-$(shell uname -m).tar.bz2 from s3"
+	aws s3 cp s3://dqlite-static-libs/latest-juju-dqlite-static-lib-deps-$(shell uname -m).tar.bz2 - | tar xjf - -C ${DQLITE_EXTRACTED_DEPS_PATH}
+
+cgo-go-op: dqlite-deps-check
+	PATH=${PATH}:/usr/local/musl/bin \
+		CC="musl-gcc" \
+		CGO_CFLAGS="-I${DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH}/include" \
+		CGO_LDFLAGS="-L${DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH} -luv -lraft -ldqlite -llz4 -lsqlite3" \
+		CGO_LDFLAGS_ALLOW="(-Wl,-wrap,pthread_create)|(-Wl,-z,now)" \
+		LD_LIBRARY_PATH="${DQLITE_EXTRACTED_DEPS_ARCHIVE_PATH}" \
+		CGO_ENABLED=1 \
+		go $o $d \
+			-mod=${JUJU_GOMOD_MODE} \
+			-tags "libsqlite3 ${BUILD_TAGS}" \
+			${COMPILE_FLAGS} \
+			-ldflags "-s -w -linkmode 'external' -extldflags '-static'" \
+			-v .
+
+cgo-go-install:
+## go-install: Install Juju binaries without updating dependencies
+	$(MAKE) cgo-go-op o=install d=
+
+cgo-go-build:
+## go-build: Build Juju binaries without updating dependencies
+	$(MAKE) cgo-go-op o=build d="-o ./bin/nu-juju-data"
---
+++ main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+
+	"github.com/SimonRichardson/nu-juju-data/repl"
+	"github.com/canonical/go-dqlite/app"
+	"github.com/canonical/go-dqlite/client"
+	"github.com/juju/clock"
+	"github.com/spf13/cobra"
+	"golang.org/x/sys/unix"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	doItLive()
+}
+
+func doItLive() {
+	var api string
+	var db string
+	var join *[]string
+	var dir string
+	var verbose bool
+
+	cmd := &cobra.Command{
+		Use:   "nu-juju-data",
+		Short: "Demo to show the nu-juju-data",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logFunc := func(l client.LogLevel, format string, a ...interface{}) {
+				if !verbose {
+					return
+				}
+				log.Printf(fmt.Sprintf("%s: %s: %s\n", api, l.String(), format), a...)
+			}
+
+			// Setup up the database.
+			app, err := app.New(dir, app.WithAddress(db), app.WithCluster(*join), app.WithLogFunc(logFunc))
+			if err != nil {
+				return err
+			}
+			if err := app.Ready(context.Background()); err != nil {
+				return err
+			}
+			db, err := app.Open(context.Background(), "demo")
+			if err != nil {
+				return err
+			}
+
+			replSock := filepath.Join(dir, "juju.sock")
+			_ = os.Remove(replSock)
+			_, err = repl.New(replSock, dbGetter{db: db}, clock.WallClock)
+			if err != nil {
+				return err
+			}
+
+			ch := make(chan os.Signal, 1)
+			signal.Notify(ch, unix.SIGPWR)
+			signal.Notify(ch, unix.SIGINT)
+			signal.Notify(ch, unix.SIGQUIT)
+			signal.Notify(ch, unix.SIGTERM)
+			signal.Notify(ch, unix.SIGKILL)
+			db.Close()
+
+			app.Handover(context.Background())
+			app.Close()
+
+			return nil
+		},
+	}
+	flags := cmd.Flags()
+	flags.StringVarP(&api, "api", "a", "", "address used to expose the demo API")
+	flags.StringVarP(&db, "db", "d", "", "address used for internal database replication")
+	join = flags.StringSliceP("join", "j", nil, "database addresses of existing nodes")
+	flags.StringVarP(&dir, "dir", "D", "/tmp/dqlite-demo", "data directory")
+	flags.BoolVarP(&verbose, "verbose", "v", false, "verbose logging")
+
+	cmd.MarkFlagRequired("api")
+	cmd.MarkFlagRequired("db")
+
+	if err := cmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+type dbGetter struct {
+	db *sql.DB
+}
+
+func (g dbGetter) GetExistingDB(_ string) (*sql.DB, error) {
+	return g.db, nil
+}
